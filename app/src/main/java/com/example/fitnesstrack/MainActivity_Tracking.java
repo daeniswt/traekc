dc8c21f90abc7b22c67f6a6c009f8f4906d7d3ec
Denise <weinert.denise.dw@gmail.com>
2022-08-08 18:05:23 +0200

    Added whole project to local git repo
    Project files relevant for the app.

---
+++ app/src/main/java/com/example/fitnesstrack/MainActivity_Tracking.java
@@ -0,0 +1,293 @@
+package com.example.fitnesstrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity_Tracking extends AppCompatActivity {
+
+    //variablen der Textanzeige
+    private int bewegerg;
+    public int mahlzeiterg;
+    private int finaleswassererg;
+    private int wassererg;
+    private int eingtrinken;
+
+
+    //Dialoge
+    Dialog AuswahlDialog;
+
+    Dialog MahlzeitDialog;
+
+    //Buttons im Hauptdialog
+    private Button moderat;
+    private Button intensiv;
+    private Button trinken;
+    private Button mahlzeit;
+
+    //Fortschrittsbalken
+    private ProgressBar fortWasser;
+    private ProgressBar fortMahlzeit;
+    private ProgressBar fortBewegung;
+
+    //Texte über den Fortschrittsbalken
+    private TextView TextViewMahlzeiten;
+    private TextView TextViewTrinken;
+    private TextView TextViewWasser;
+    private TextView TextViewBewegung;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AuswahlDialog = new Dialog(this);
+
+
+        moderat = findViewById(R.id.moderat);
+        intensiv = findViewById(R.id.intensiv);
+        trinken = findViewById(R.id.trinken);
+        mahlzeit = findViewById(R.id.nahrung);
+
+        //INIT
+        int wassererg = 0;
+        int mahlzeiterg = 0;
+
+        TextViewMahlzeiten = findViewById(R.id.wertanzeigeMahlzeiten);
+        TextViewMahlzeiten.setText("3 / " + String.valueOf(mahlzeiterg));
+        TextViewWasser = findViewById(R.id.wertanzeigeWasser);
+
+        TextViewBewegung = findViewById(R.id.wertanzeigeBewegung);
+
+
+
+
+
+        FloatingActionButton addA = findViewById(R.id.addAktivitaet);
+        FloatingActionButton removeA = findViewById(R.id.removeAktivitaet);
+
+
+
+        //ON-CLICK für die Floating Buttons
+
+        addA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Starten der Methode, die das Auswahlfeld für einen Eintrag öffnet
+                aktivitaetauswahl();
+            }
+        });
+
+        removeA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //Methode, um den letzten eintrag rückgängig zu machen
+            }
+        });
+
+    }
+
+    //Methoden für die Dialoge:
+
+    public void aktivitaetauswahl() {
+        //methode, um den Hauptdialog zu starten &
+        //um die Funktionen zuzuweisen
+
+        //Dialog eine COntentView zuweisen, hier das layout für das erste Pop-Up Fenster
+        AuswahlDialog.setContentView(R.layout.popup_aktivitaetswahl);
+
+        moderat = AuswahlDialog.findViewById(R.id.moderat);
+        intensiv = AuswahlDialog.findViewById(R.id.intensiv);
+        trinken = AuswahlDialog.findViewById(R.id.trinken);
+        mahlzeit = AuswahlDialog.findViewById(R.id.nahrung);
+
+
+
+
+
+
+
+        mahlzeit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuswahlDialog.dismiss();
+                ZeigMahlzeitDialog();
+            }
+        });
+
+        intensiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuswahlDialog.dismiss();
+                ZeigIntensivDialog();
+            }
+        });
+
+        moderat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuswahlDialog.dismiss();
+                ZeigModeratDialog();
+            }
+        });
+
+        trinken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuswahlDialog.dismiss();
+                ZeigWasserDialog();
+
+                //warten auf Usereingabe hier?
+
+                //TextViewTrinken.setText(
+                        //getString(R.string.wasserpref).concat(String.valueOf(finaleswassererg)));
+
+
+
+            }
+        });
+
+        AuswahlDialog.show();
+    }
+
+
+    //--Dialoge für die einzelnen Buttons + Funktion + Wertzuweisung -----------------------------
+
+    public void ZeigWasserDialog() {
+        Dialog WasserDialog = new Dialog(this);
+        WasserDialog.setContentView(R.layout.poput_eingabewasser);
+        WasserDialog.setTitle("titel");
+
+        EditText eingWasser = WasserDialog.findViewById(R.id.eingabeWasser);
+
+
+        Button wasserok = WasserDialog.findViewById(R.id.bestätigewasser);
+        Button wasserabbr = WasserDialog.findViewById(R.id.abbruchwasser);
+
+                wasserok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        //eingtrinken = Integer.parseInt(eingWasser.getText().toString());
+
+                    }
+                });
+
+            wasserabbr.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        WasserDialog.dismiss();
+                     }
+                });
+
+
+        WasserDialog.show();
+
+
+
+
+
+
+
+    }
+
+    //-------------------------------------------------------------------------
+
+    public void ZeigIntensivDialog() {
+        //platz für bewegendialog
+    }
+
+    public void ZeigModeratDialog() {
+        //platz für bewegendialog
+    }
+
+    public void ZeigMahlzeitDialog() {
+        this.mahlzeiterg=mahlzeiterg;
+        Dialog MahlzeitDialog = new Dialog(this);
+        MahlzeitDialog.setContentView(R.layout.popup_mahlzeit);
+
+        Button schliessen = MahlzeitDialog.findViewById(R.id.bestätigemahlzeit);
+
+
+        mahlzeit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mahlzeiterg=+ 1;
+
+                //Dialog nachdem inkrementirt wurde als bestätigung
+
+                schliessen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        MahlzeitDialog.dismiss();
+                    }
+                });
+
+            }
+
+
+
+        });
+
+        //mahlzeiterg mit klick auf button um 1 inkrementieren
+
+
+        MahlzeitDialog.show();
+
+
+    }
+
+    //keinen Dialog für mahlzeiten weil diese immer nur um 1 erhöht wird - Umsetzung?
+
+
+    //--Toolbar Menü -----------------------------------------------------------------
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_home) {
+
+            //zurückwechseln zum tracker wenn in anderem menüpunkt
+
+        }
+
+        if (id == R.id.menu_tipps) {
+
+            Intent wechselzutipps = new Intent(this, ActivityTipps.class);
+            startActivity(wechselzutipps);
+
+            //Methode startActivity mit intent, activity mit tipps
+
+
+            //startActivity(new Intent(MainActivity_Tracking.this, ActivityTipps.class));
+            //neues Intent, indem die zweite aktivität aufgerufen wird
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
