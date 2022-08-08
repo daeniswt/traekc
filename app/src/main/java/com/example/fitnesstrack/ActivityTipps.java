cb7c43af40b44e5379b3a3e9a0a5b0ceea25ef5e
Denise <weinert.denise.dw@gmail.com>
2022-08-08 18:05:23 +0200

    Added whole project to local git repo
    Project files relevant for the app.

---
+++ app/src/main/java/com/example/fitnesstrack/ActivityTipps.java
@@ -0,0 +1,14 @@
+package com.example.fitnesstrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityTipps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipps);
+    }
+}
